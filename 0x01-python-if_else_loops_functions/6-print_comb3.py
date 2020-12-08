baccf74ac28a712d84b890e16ecdc302cdbfffbd
Chloe <2106@holbertonschool.com>
2020-12-08 16:17:05 +0000

    pep8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for first in range(0, 10):
     second = first + 1
-    for second in range (second, 10):
-        print("{:d}".format(first), end = '')
+    for second in range(second, 10):
+        print("{:d}".format(first), end='')
         if first == 8 and second == 9:
             print("{:d}".format(second))
         else:
-            print("{:d}, ".format(second), end = '')
+            print("{:d}, ".format(second), end='')
