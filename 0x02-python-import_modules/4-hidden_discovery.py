ed849d78a80b9849ffdaabb1a554e8dd25cfa664
Chloe <2106@holbertonschool.com>
2020-12-10 17:23:36 +0000

    task 5 2 git push

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    token = dir(hidden_4)
-    for name in range(len(token)):
-        if token[name][:2] != '__':
-            print(token[name])
+    for name in range(len(dir(hidden_4))):
+        if dir(hidden_4)[name][:2] != '__':
+            print("{}".format(dir(hidden_4)[name]))
