8d8017083b45a8c47309eb6689bd7903ab61cdd3
Chloe <2106@holbertonschool.com>
2020-12-15 15:20:00 +0000

    pep8 fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for n in range(len(my_list)):
-        print(my_list[n], sep="\n")
+    for n in range my_list:
+        print("{:d}".format(n))
