4678bdb3d6ec6e55e1b36f20d11cc8c1095efadf
Chloe <2106@holbertonschool.com>
2020-12-15 15:08:57 +0000

    task10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            list1 = ["True"]
+            return list1
+        else:
+            list1 = ["False"]
+            return list1
