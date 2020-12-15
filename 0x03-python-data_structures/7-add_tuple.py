a319bb2a8c3cabbdf1f134fb156cd51dabde45e0
Chloe <2106@holbertonschool.com>
2020-12-15 14:50:42 +0000

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if tuple_a is None or tuple_b is None:
+            return
+    first = tuple_a + ('0', '0')
+    second = tuple_b + ('0', '0')
+    
+    sum1 = int(first[0]) + int(second[0])
+    sum2 = int(first[1]) + int(second[1])
+    new_tuple = (sum1, sum2)
+    return new_tuple
