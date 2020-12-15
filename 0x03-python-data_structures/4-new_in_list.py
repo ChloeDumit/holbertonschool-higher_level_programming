66005f1886f3b2edbc524bcbb62cf939be57f16e
Chloe <2106@holbertonschool.com>
2020-12-15 15:29:53 +0000

    pep8

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 def new_in_list(my_list, idx, element):
     listcopy = my_list.copy()
     if idx < 0:
-        return(listcopy)
-    if idx > len(my_list):
-        return(listcopy)
+        return(my_list)
+    if idx >= len(my_list):
+        return(my_list)
     listcopy[idx] = element
     return(listcopy)
