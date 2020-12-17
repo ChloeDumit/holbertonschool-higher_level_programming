6bb8a7cf01620eaa4eb3751a3f5c05f03c4e2de2
Chloe <2106@holbertonschool.com>
2020-12-17 13:49:00 +0000

    pep8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     return list(map(square, matrix))
 
-def square(num=[]):
-    return list(map(lambda x : (x * x), num))
 
+def square(num=[]):
+    return list(map(lambda x: (x * x), num))
