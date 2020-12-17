3811e6c9d40a8831727a8d0432da6bdcfef4ad2c
Chloe <2106@holbertonschool.com>
2020-12-17 17:13:31 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return
-    n = []
-    for new in a_dictionary:
-        n.append(new)
-    return(max(n))
+    if a_dictionary is None:
+            return None
+    max_score = 0
+    key = None
+    for k, v in a_dictionary.items():
+        if v > max_score:
+            max_score = v
+            key = k
+    return key
