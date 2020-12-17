40353fb5bc138392728e0e88b98a44869bb5aec0
Chloe <2106@holbertonschool.com>
2020-12-17 16:16:57 +0000

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    copy = {k: v * 2 for k, v in a_dictionary.items()}
-    return copy
+    new = a_dictionary.copy()
+    for k in new:
+        new[k] *=2
+    return new
+
 
