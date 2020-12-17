00b9bf434da7df71697224c855a1f8550bbd9bb2
Chloe <2106@holbertonschool.com>
2020-12-17 18:20:45 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
     if type(roman) is not str or len(roman) is 0:
         return 0
     for item in range(item, len(roman)):
-        if item < len(roman) -1 and dic[roman[item]] < dic[roman[item + 1]]:
+        if item < len(roman) - 1 and dic[roman[item]] < dic[roman[item + 1]]:
             result -= dic[roman[item]]
         else:
             result += dic[roman[item]]
