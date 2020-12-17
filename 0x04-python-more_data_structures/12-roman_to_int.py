d5a026110b3dd74e5bfe82119b4e9ccd357a84ad
Chloe <2106@holbertonschool.com>
2020-12-17 18:16:56 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,37 +1,14 @@
 #!/usr/bin/pyhton3
-def values(i):
-    if i == 'I':
-            return 1
-    elif i == 'V':
-            return 5
-    elif i == 'X':
-            return 10
-    elif i == 'L':
-            return 50
-    elif i == 'C':
-            return 100
-    elif i == 'D':
-            return 500
-    elif i == 'M':
-            return 1000
-    return -1
-
 def roman_to_int(roman_string):
-    res = 0
-    i = 0
-    if roman_string is None:
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    item = 0
+    roman = roman_string
+    if type(roman) is not str or len(roman) is 0:
         return 0
-    while i < len(roman_string):
-        s1 = values(roman_string[i])
-        if i + 1 < len(roman_string):
-            s2 = values(roman_string[i + 1])
-            if s1 >= s1:
-                res = res + s1
-                i = i + 1
-            else:
-                res = res + s2 - s1
-                i = i + 2
+    for item in range(item, len(roman)):
+        if item < len(roman) -1 and dic[roman[item]] < dic[roman[item + 1]]:
+            result -= dic[roman[item]]
         else:
-            res = res + s1
-            i = i + 1
-    return (res)
+            result += dic[roman[item]]
+    return result
