7197703f65c8a96baf7d56abaa0b6ceb6de1a2b6
Chloe <2106@holbertonschool.com>
2020-12-17 17:02:53 +0000

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key: value})
-    return a_dictionary
+   new_list = a_dictionary
+   new_list[key] = value
+   return(new_list)
