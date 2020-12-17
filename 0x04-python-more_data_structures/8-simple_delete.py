e0cd45422a14946378ea655c54740bed1628c66f
Chloe <2106@holbertonschool.com>
2020-12-17 17:06:35 +0000

    task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.get(key) is not None and a_dictionary.pop(key)
-    return(a_dictionary)
+   new_dic = a_dictionary
+   new_dic.pop(key, None)
+   return new_dic
