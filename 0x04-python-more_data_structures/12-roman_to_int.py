657d92b86043081a39d86258450b5197f2bcc663
Chloe <2106@holbertonschool.com>
2020-12-17 18:21:58 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/pyhton3
 def roman_to_int(roman_string):
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    
     result = 0
     item = 0
     roman = roman_string
