f7615961f24bfef132688a8b19210b31ffcabba3
Chloe <2106@holbertonschool.com>
2021-01-04 16:10:26 +0000

    upload

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    new_str = ""
+    for i in my_list[:x]:
+        new_str += str(i)
+        n += 1
+    try:
+        print(new_str)
+        return n
+    except:
+        print("Error")
+    
