997cf38059d7ed7ed7ffdbd2150af8937b418f6e
Chloe <2106@holbertonschool.com>
2021-01-05 18:31:14 +0000

    comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 """
 Class Square: creates a square class
 """
+
 class Square:
     """create class"""
     pass
---
+++ 0x06-python-classes/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+MagicClass = __import__('103-magic_class').MagicClass
+
+magic = MagicClass(5)
+print(magic)
