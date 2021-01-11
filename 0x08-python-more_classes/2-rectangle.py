f1e1a92ce12f58d5612e1c2c63a04a49192f20a0
Chloe <2106@holbertonschool.com>
2021-01-11 17:43:53 +0000

    first upload

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!usr/bin/python3
+"""
+Create a rectangle class
+"""
+
+
+class Rectangle:
+        """ Create an empty rectangle class"""
+        def __init__(self, width=0, height=0):
+            """ Initialize data"""
+            self.width = width
+            self.height = height
+
+        @property
+        def height(self):
+            return(self.__height)
+
+        @height.setter
+        def height(self, value):
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__height = value
+
+        @property
+        def width(self):
+            return(self.__width)
+
+        @width.setter
+        def width(self, value):
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+        def area(self):
+            """Calculates Area"""
+            return (self.__height * self.__width)
+
+        def perimeter(self):
+            """Calculates Perimeter"""
+            if self.__width == 0 or self.__height == 0:
+                return 0
+            else:
+                return (self.__height + self.__width) * 2
---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,66 @@
+#!usr/bin/python3
+"""
+Create a rectangle class
+"""
+
+
+class Rectangle:
+        """ Create an empty rectangle class"""
+        def __init__(self, width=0, height=0):
+            """ Initialize data"""
+            self.width = width
+            self.height = height
+
+        @property
+        def height(self):
+            return(self.__height)
+
+        @height.setter
+        def height(self, value):
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__height = value
+
+        @property
+        def width(self):
+            return(self.__width)
+
+        @width.setter
+        def width(self, value):
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+        def area(self):
+            """Calculates Area"""
+            return (self.__height * self.__width)
+
+        def perimeter(self):
+            """Calculates Perimeter"""
+            if self.__width == 0 or self.__height == 0:
+                return 0
+            else:
+                return (self.__height + self.__width) * 2
+
+        def __str__(self):
+            result = ""
+
+            if self.__width == 0 or self.__height == 0:
+                return result
+            else:
+                for i in range(self.__height):
+                    for j in range(self.__width):
+                        result = result + "#"
+                    result = result + '\n'
+                result = result[:-1]
+                return result
+
+        def __repr__(self):
+            return "Rectangle(" + str(self.__width) + \
+                "," + str(self.__height) + ")"
