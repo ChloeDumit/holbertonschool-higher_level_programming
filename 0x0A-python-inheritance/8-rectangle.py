f5a2d653144c427498c3eb597cf64ce56f750e64
Chloe <2106@holbertonschool.com>
2021-01-20 00:15:12 +0000

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,18 @@
+  
 #!/usr/bin/python3
-""" importing """
+""" Write a class BaseGeometry (based on 6-base_geometry.py). """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-Return true if object is an instance
-of a class
-"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+""" Program that creates a Rectangle and intance its values """
 
 
 class Rectangle(BaseGeometry):
-    """ rectangle class """
+    """ the inherit from BaseGeometry """
     def __init__(self, width, height):
-        """initalize data """
+        """ init function """
         self.__width = width
         self.__height = height
-        self.integer_validator(self, 'width', width)
-        self.integer_validator(self, 'height', height)
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
