8b4696ec17fbc2f5b0ee05684b9b91769b33fac3
Chloe <2106@holbertonschool.com>
2021-01-20 19:51:36 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
 
-reads a text file
+reads a text file (UTF8) and prints it to stdout
 
 """
 
 
 def read_file(filename=""):
-    """ reads file """
+    """ reads a text file (UTF8) and prints it to stdout """
     with open(filename) as f:
         for line in f:
             print(line, end="")
