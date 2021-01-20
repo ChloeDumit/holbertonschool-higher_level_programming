e7f616047b6c51b66a7e54d6558414fcf693d366
Chloe <2106@holbertonschool.com>
2021-01-20 00:14:37 +0000

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ importing """
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 Return true if object is an instance
@@ -10,8 +11,7 @@ of a class
 class Rectangle(BaseGeometry):
     """ rectangle class """
     def __init__(self, width, height):
-        """initalize data
-        """
+        """initalize data """
         self.__width = width
         self.__height = height
         self.integer_validator(self, 'width', width)
