d6aeb6944901d119a677f258158d31e242a7a04f
Chloe <2106@holbertonschool.com>
2021-01-24 14:08:36 +0000

    until 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,6 +22,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ convert to json string """
         lista = []
@@ -31,6 +32,7 @@ class Base:
         else:
             return lista
 
+    @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of
         list_objs to a file"""
@@ -42,3 +44,35 @@ class Base:
         jstr = cls.to_json_string(my_list)
         with open(fname, 'w') as f:
             f.write(jstr)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation"""
+        my_list = []
+        if json_string is None:
+            return my_list
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set """
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1,2)
+        if cls.__name__ == 'Square':
+            dummy = cls(7)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns list of instances """
+        lista = []
+        name = cls.__name__ + ".json"
+        if os.path.isfile(name):
+            with open(name, 'r') as f:
+                readstr = f.read()
+                stringread = cls.from_json_string(readstr)
+                for i in stringread:
+                    lista.append(cls.create(**i))
+        return lista
