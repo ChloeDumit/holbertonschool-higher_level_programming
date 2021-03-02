c0ae764896f663f3e31fc54b44b06988a15c55fc
ChloeDumit <2106@holbertonschool.com>
2021-03-02 18:30:22 +0000

    task 14

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,7 @@
 -- list join --
 
 SELECT t.title, tg.genre_id
-FROM tv_shows t RIGHT OUTER JOIN tv_show_genres tg
+FROM tv_shows_genres tg
+RIGHT OUTER JOIN tv_shows t
 ON t.id = tg.show_id
 ORDER BY t.title, tg.genre_id ASC;
