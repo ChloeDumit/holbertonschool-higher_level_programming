4abcd3b080418be17934db946a5070f1f49a8db8
ChloeDumit <2106@holbertonschool.com>
2021-03-02 17:08:16 +0000

    uplaoad

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 
 SELECT t.title, tg.genre_id
 FROM tv_shows t LEFT JOIN tv_show_genres tg
-ON t.id = tg.genre_id
+ON t.id = tg.show_id
 WHERE tg.genre_id IS NOT NULL
 ORDER BY t.title, tg.genre_id;
