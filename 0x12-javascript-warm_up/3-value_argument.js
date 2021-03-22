f9049037b169518ac5dc3981d59505fb3e6bbb4d
ChloeDumit <2106@holbertonschool.com>
2021-03-22 14:15:56 +0000

    upload

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let size;
+let arg=[];
+
+process.argv.forEach((val, index) => {
+  size = `${index}`;
+  arg[index] = `${val}`;
+});
+if (size <= 2)
+{
+  console.log('No argument');
+}else {
+  console.log(arg[2]);
+}
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+let arg1, arg2;
+arg1 = process.argv[2];
+arg2 = process.argv[3];
+console.log (arg1 + ' '+ 'is' + ' ' + arg2);
