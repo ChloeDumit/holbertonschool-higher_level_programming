aa7c3bd115166598abc87869334ee7239cd09196
ChloeDumit <2106@holbertonschool.com>
2021-03-22 15:42:54 +0000

    task 92

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+function add(a, b) {
+    const result = a + b;
+    console.log(result);
+}
+
+add(arg1, arg2);
