0bbea560bdd530b905b26a35bf1b18086f9ea603
ChloeDumit <2106@holbertonschool.com>
2021-03-23 18:01:56 +0000

    task 72

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
     let count = 0;
-    if (searchElement != undefined && list != undefined) {
     for(let i = 0; i < list.length; i++) {
-        if (searchElement == list[i]) {
+        if (list[i] == searchElement) {
             count++;
         }
     }
-}
     return count;
 }
