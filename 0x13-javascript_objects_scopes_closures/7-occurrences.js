2ab3c8153341efbdfc671c1f4d378a62e81e6ac6
ChloeDumit <2106@holbertonschool.com>
2021-03-23 17:58:18 +0000

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
     let count = 0;
-    if (searchElement != undefined) {
+    if (searchElement != undefined && list != undefined) {
     for(let i = 0; i < list.length; i++) {
         if (searchElement == list[i]) {
             count++;
