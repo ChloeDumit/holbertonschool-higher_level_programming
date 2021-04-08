3ca6dfd77edf436e185b0cce826da53a992f22df
ChloeDumit <2106@holbertonschool.com>
2021-04-08 18:02:04 +0000

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Test function find_peak """
 def find_peak(list_of_integers):
+    """function peak"""
+    
     if list_of_integers is None or list_of_integers == []:
         return None
     low = 0
