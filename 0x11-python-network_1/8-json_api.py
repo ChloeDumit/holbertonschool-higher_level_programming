d1d48420eb643b5bffc9db18ddd67d3c0d466d73
ChloeDumit <2106@holbertonschool.com>
2021-04-09 15:14:04 +0000

    pep8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,12 +5,12 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    
+
     if (len(argv) > 1):
         letter = argv[1]
     else:
         letter = ""
-        
+
     data = {'q': letter}
     r = requests.post('http://0.0.0.0:5000/search_user', data=data)
     try:
@@ -21,4 +21,3 @@ if __name__ == "__main__":
             print("No result")
     except:
         print("Not a valid JSON")
-        
