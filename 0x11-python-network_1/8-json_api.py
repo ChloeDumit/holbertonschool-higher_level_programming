55104674eefa8aafdcb72d3656d47d3ec3b4e66e
ChloeDumit <2106@holbertonschool.com>
2021-04-09 14:53:31 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,18 +6,19 @@ from sys import argv
 
 if __name__ == "__main__":
     
-    if (len(argv) < 1):
-        letter = ""
-    else:
+    if (len(argv) > 1):
         letter = argv[1]
-data = {'q': letter}
-r = requests.post('http://0.0.0.0:5000/search_user', data=data)
-try:
-    body = r.json()
-    if (len(body) != 0):
-        print("[{}] {}".format(body['id'], body['name']))
     else:
-        print("No result")
-except:
-    print("Not a valid JSON")
-    
+        letter = ""
+        
+    data = {'q': letter}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    try:
+        body = r.json()
+        if (len(body) != 0):
+            print("[{}] {}".format(body['id'], body['name']))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
+        
